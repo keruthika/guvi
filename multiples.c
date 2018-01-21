e168c338226d6e184ca045b28282744605d01c56
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-21 10:36:46 +0530

    Create multiples.c

---
+++ multiples.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int sum=0,i,n;
+ clrscr();
+ printf("enter a number ");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  printf("\n 5 * %d = %d",i,5*i);
+  }        
+  getch();
+  }
+           
+        
+        
