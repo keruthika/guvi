ad51e97cb10e9398977bab28dc7abfdf25c05b73
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-21 09:56:23 +0530

    Create even interval.c

---
+++ even interval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+           int f1,f2,r,i;
+           printf("enter the First number");
+           scanf("%d",&f1);
+           printf("enter the last number");
+           scanf("%d",&f2);
+           printf("The Even numbers between f1,f2);
+           for(i=f1; i<=f2; ++i)
+           {
+               r = i % 2;
+               if(rem == 0)
+               printf("\n  %d",i);
+           }
+    return 0; 
+    } 
